4806bcfdb2407e949484d4e52cfb52ddb422258c
MichaelPoly <polygalov_m@rambler.ru>
2017-05-19 12:48:31 +0300

    Домашнее задание по второму уроку.

---
+++ JS/homeWork.js
@@ -0,0 +1,119 @@
+
+//Пункт 2
+var siteName = "siteName";
+var siteMenu = ["link1","link2","link3","link4","link5"];
+var articleQuantity =5;
+var articleHeader = "Header";
+var articleNormal;
+var articleHot;
+var articleHumor;
+var articleSerious;
+var isImage = true;
+var price = 1000;
+
+// Пункт3 - Ошибки исправил
+sad = 'This is Sparta!!!'
+
+result = true;  //Значение есть результат или нет. Если есть true, если значения нет
+                 // false
+var entage = 20;
+var header = "Заголовок новой статьи";
+var zip_code = 983456;
+prompt('Введите хоть что-нибудь');
+alert ('Вы точно все ввели?');
+var var1 = 25;
+var var2 = 'Орехов';
+alert ('Вы нашли' + var1 + var2);
+var var3 = 45;
+var var4 = 20;
+//В консоли проверил. результат var1+var2 = 65
+
+//Пункт 4
+
+var art = "25 / 5";
+var same = null;
+var scoop;
+var flag = 0;
+var plat = true;
+var devid = 34 / 7;
+var person = {lastname: Ivan}
+var fruits = ['apple'];
+var iceCream = function() { alert('Я люблю мороженое'); }
+/* результат из консоли
+typeof art;
+"string"
+typeof same;
+"object"
+typeof scoop;
+"undefined"
+typeof flag;
+"number"
+typeof plat;
+"boolean"
+typeof devid;
+"number"
+typeof person;
+"undefined"
+typeof fruits;
+"undefined"
+typeof iceCream;
+"undefined"
+*/
+
+//Пункт 5
+var str1 = "JavaScript is a high-level, dynamic, untyped, and interpreted programming language";
+var str2 = "Netscape Communications realized that the Web needed to become more dynamic";
+var str3 = "JavaScript was realized in ten days";
+
+var str1Legth = str1.length;
+var str2Legth = str2.length;
+var str3Legth = str3.length;
+/* результат из консоли
+str1Legth
+82
+str2Legth
+75
+str3Legth
+35
+*/
+
+//Пункт 6
+var str4 = "console.log";
+var str5 = 'Hello World!';
+var str6 = 'I need more code on JavaScript';
+
+var str4Ucase = str4.toUpperCase();
+var str5Ucase = str5.toUpperCase();
+var str6Ucase = str6.toUpperCase();
+/* результат из консоли
+str4Ucase
+"CONSOLE.LOG"
+str5Ucase
+"HELLO WORLD!"
+str6Ucase
+"I NEED MORE CODE ON JAVASCRIPT"
+*/
+
+//Пункт 7
+var str4Ucase = str4.toLowerCase();
+var str5Ucase = str5.toLowerCase();
+var str6Ucase = str6.toLowerCase();
+/* результат из консоли
+str4Ucase
+"console.log"
+str5Ucase
+"hello world!"
+str6Ucase
+"i need more code on javascript"
+*/
+var str4Ucase = str4Ucase.slice(0,1).toUpperCase() + str4Ucase.slice(1);
+var str5Ucase = str5Ucase.slice(0,1).toUpperCase() + str5Ucase.slice(1);
+var str6Ucase = str6Ucase.slice(0,1).toUpperCase() + str6Ucase.slice(1);
+/* результат из консоли
+str4Ucase
+"Console.log"
+str5Ucase
+"Hello world!"
+str6Ucase
+"I need more code on javascript"
+*/
