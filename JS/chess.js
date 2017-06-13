c2ae682f010556d2535bc66a4ec1299345cc9203
MichaelPoly <polygalov_m@rambler.ru>
2017-06-13 12:11:28 +0300

    Доделал отрисовку шахматной доски

--- JS/chess.js
+++ JS/chess.js
@@ -6,7 +6,7 @@ function chess() {
     att1.value = char;
     div1.setAttributeNode(att1);
     var style = document.createAttribute('style');
-    style.value = 'width: 100%; height: 102px; border: 1px solid blue; display: flex; display: -webkit-flex; flex-direction: row; justify-content: center;';
+    style.value = 'width: 100%; height: 102px; display: flex; display: -webkit-flex; flex-direction: row; justify-content: center; margin-left: 3%;';
     div1.setAttributeNode(style);
     document.querySelector('section.center1').appendChild(div1);
   }
@@ -18,21 +18,51 @@ function chess() {
   raw('F');
   raw('G');
   raw('H');
+
+function drawDiv1(rawName) {
   for (var i = 1; i < 9; i++) {
     var div = document.createElement('div');
     var att = document.createAttribute('id');
-    att.value = 'A' + i;
+    att.value = rawName + i;
     div.setAttributeNode(att);
     var style = document.createAttribute('style');
       if (i/2 - Math.round(i/2) != 0) {
-        style.value = 'width: 100px; height: 100px; background-color: black; border: 1px solid blue;';
+        style.value = 'width: 100px; height: 100px; background-color: #8c5e07; border: 1px solid black;';
       } else {
-        style.value = 'width: 100px; height: 100px; background-color: wite; border: 1px solid blue;';
+        style.value = 'width: 100px; height: 100px; background-color: wite; border: 1px solid black;';
       }
     div.setAttributeNode(style);
-    document.querySelector('section.center1').querySelector('div').appendChild(div);
+    var divRawName = 'div#' + rawName;
+    document.querySelector('section.center1').querySelector(divRawName).appendChild(div);
 
   }
+}
+function drawDiv2(rawName) {
+  for (var i = 1; i < 9; i++) {
+    var div = document.createElement('div');
+    var att = document.createAttribute('id');
+    att.value = rawName + i;
+    div.setAttributeNode(att);
+    var style = document.createAttribute('style');
+      if (i/2 - Math.round(i/2) == 0) {
+        style.value = 'width: 100px; height: 100px; background-color: #8c5e07; border: 1px solid black;';
+      } else {
+        style.value = 'width: 100px; height: 100px; background-color: wite; border: 1px solid black;';
+      }
+    div.setAttributeNode(style);
+    var divRawName = 'div#' + rawName;
+    document.querySelector('section.center1').querySelector(divRawName).appendChild(div);
+
+  }
+}
+drawDiv1('A');
+drawDiv2('B');
+drawDiv1('C');
+drawDiv2('D');
+drawDiv1('E');
+drawDiv2('F');
+drawDiv1('G');
+drawDiv2('H');
 
 
 
